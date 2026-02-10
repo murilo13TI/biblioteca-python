cc4d3a55e68962458fd5104983816c03fc897305
murilo almeida <muriloalmeidasilva73@gmail.com>
2026-02-10 11:21:17 -0300

    Add files via upload

--- funcao.py
+++ funcao.py
@@ -1,7 +1,30 @@
 from flask import Flask, jsonify, request
 from main import app, con
+def verificar_senha(senha):
 
-@app.route('/cadastrar/', methods=['POST'])
-def cadastro():
+    if len(senha) < 8:
+        return False
 
-    try:
+    tem_maiuscula = False
+    tem_minuscula = False
+    tem_numero = False
+    tem_especial = False
+
+    caracteres_especiais = '!@#$%&*§^~?ªº'
+
+    for char in senha:
+        if char.isupper():
+            tem_maiuscula = True
+        elif char.islower():
+            tem_minuscula = True
+        elif char.isdigit():
+            tem_numero = True
+        elif char in caracteres_especiais:
+            tem_especial = True
+
+    if tem_maiuscula and tem_minuscula and tem_numero and tem_especial:
+        return True
+
+    return False
+
+
--- view.py
+++ view.py
@@ -1,5 +1,7 @@
 from flask import Flask, jsonify, request
 from main import app, con
+from funcao import verificar_senha
+from flask_bcrypt import generate_password_hash, check_password_hash
 
 @app.route('/listar_livro', methods=['GET'])
 def listar_livro():
@@ -26,7 +28,7 @@ def listar_livro():
         return jsonify({'message': f'Erro ao consultar o banco de dados: {e}'}), 500
 
     finally:
-        cur.close()
+        con.close()
 
 @app.route('/criar_livro', methods=['POST'])
 def criar_livro():
@@ -59,7 +61,7 @@ def criar_livro():
 
         return jsonify({'message': 'Erro ao criar o livro'}), 500
     finally:
-        cur.close()
+        con.close()
 
 @app.route('/editar_livro/<int:id>', methods=['PUT'])
 def editar_livro(id):
@@ -107,3 +109,118 @@ def deletar_livro(id):
     cur.close()
 
     return jsonify({"message": "livro deletado com sucesso!", 'id_livro': id})
+
+@app.route('/cadastro', methods=['POST'])
+def cadastro():
+    cur = con.cursor()
+
+    try:
+        dados = request.get_json()
+
+        nome_usuario = dados.get('nome_usuario')
+        email = dados.get('email')
+        senha = dados.get('senha')
+
+
+        cur.execute("SELECT id_usuario FROM usuario WHERE email = ?", (email,))
+        usuario = cur.fetchone()
+
+        if usuario:
+            return jsonify({'error': 'Já existe uma conta com este email'}), 400
+
+        if not verificar_senha(senha):
+            return jsonify({"erro": "A senha não atinge os requisitos necessários"}), 400
+
+        hash_senha = generate_password_hash(senha).decode('utf-8')
+
+        cur.execute(
+            "INSERT INTO usuario (nome_usuario, email, senha) VALUES (?, ?, ?)",
+            (nome_usuario, email, hash_senha)
+        )
+
+        con.commit()
+        return jsonify({"message": "Cadastro feito com sucesso!"}), 201
+
+    except Exception as e:
+        print(e)  # ajuda MUITO no debug
+        return jsonify({"error": "Não foi possível a criação da conta"}), 500
+
+    finally:
+        cur.close()
+
+@app.route('/login', methods=['GET'])
+def login():
+    cur = con.cursor()
+    try:
+        dados = request.get_json()
+
+        email = dados.get('email')
+        senha = dados.get('senha')
+
+        cur.execute("select id_usuario , nome , email, senha from usuario where = ?", (email,))
+
+
+        if email != email and check_password_hash(senha) != senha:
+            return jsonify({'error': 'O email ou a senha não se coincidem'})
+
+        con.close()
+
+        if email == email and check_password_hash(senha) == senha:
+            return jsonify({'message':'Usuário logado com sucesso'})
+            cur.close()
+
+    except Exception as e:
+        return jsonify({'error':'Não conseguimos logar a conta'})
+    finally:
+        con.close()
+
+@app.route('/edita_usuario', methods=['PUT'])
+def editar_usuario(id):
+
+    cur = con.cursor()
+    cur.execute("""select id_usuario, nome_usuario, email, senha from usuario where id_usuario = ?""", (id,))
+
+    usuario_existe = cur.fetchone()
+
+    if not usuario_existe:
+        return jsonify({'error':'Usuario não encontrado'}), 404
+        cur.close()
+
+    else:
+        dados = request.get_json()
+        nome_usuario = dados.get('nome_usuario')
+        email = dados.get('email')
+        senha = dados.get('senha')
+
+        cur.execute("""update livro set nome_usuario = ?, email = ? , senha = ? where id_usuario = ?""",
+                    (nome_usuario, email, senha, id))
+
+        con.commit()
+        cur.close()
+
+        return jsonify({"message": "Usuario atualizado com sucesso!",
+                        "livro": {
+                            "id_usuario": id,
+                            "nome_usuario": nome_usuario,
+                            "email": email,
+                            "senha": senha
+                        }
+                        })
+
+@app.route('/deletar_usuario', methods=['DELETE'])
+def deletar_usuario():
+    cur = con.cursor()
+
+    cur.execute('select 1 from usuario where id_usuario = ?', (id,))
+    if not cur.fetchone():
+        cur.close()
+        return jsonify({"error": " Usuario não encontrado"}), 404
+
+    cur.execute('delete from usuario where id = ?', (id,))
+    con.commit()
+    cur.close()
+
+    return jsonify({"message": "Usuario deletado com sucesso!", 'id_usuario': id})
+
+
+
